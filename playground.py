fd03ae0bd3f5ec34f3f95c1eda5a828a64d18155
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-10-23 21:56:14 -0400

    Initial commit with data set and basic scripts

---
+++ playground.py
@@ -0,0 +1,28 @@
+import os
+import json
+from pprint import pprint
+
+def count_types(pokedex):
+    types = {}
+    for pokemon in pokedex:
+        type = pokemon['primary_type']
+        #type = pokemon['secondary_type']
+        if type in types:
+            types[type] += 1
+        else:
+            types[type] = 1
+
+    return types
+
+
+def main() -> None:
+    dir = os.path.dirname(p=os.path.realpath(filename=__file__))
+    input_path = 'pokemon_list.json'
+
+    with open(os.path.join(dir, input_path), mode='r') as data_file:
+        pokedex = json.load(data_file)
+        pprint(count_types(pokedex))
+
+
+if __name__ == "__main__":
+    main()
